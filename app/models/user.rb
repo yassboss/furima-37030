c498332b0a65be0ad598b7c6f24b85ddcf26f07f
yassboss <kyh58221022@gmail.com>
2021-12-12 23:08:39 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname, :birthday
+    validates :last_name, :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください'}
+    validates :last_name_reading, :first_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: '全角（カタカナ）を使用してください'}
+  end
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字混合で入力してください'}
 end
