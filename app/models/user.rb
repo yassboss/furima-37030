d2decda7300ba5ef4fa3235f578b0dfad96d161e
yassboss <kyh58221022@gmail.com>
2021-12-13 22:40:26 +0900

    重複エラーメッセージ非表示の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  NAME_READING_REGEX = /\A[ァ-ヶー]+\z/.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 
   with_options presence: true do
     validates :nickname, :birthday
-    validates :last_name, :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください'}
-    validates :last_name_reading, :first_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: '全角（カタカナ）を使用してください'}
+    validates :last_name, :first_name, format: {with: NAME_REGEX, message: 'には全角（漢字・ひらがな・カタカナ）を使用してください', allow_blank: true}
+    validates :last_name_reading, :first_name_reading, format: {with: NAME_READING_REGEX, message: 'には全角（カタカナ）を使用してください', allow_blank: true}
   end
-  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字混合で入力してください'}
+
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', allow_blank: true
 end
