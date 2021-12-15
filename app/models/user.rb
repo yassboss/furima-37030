5f40cf9b922d08304cee93b9671783d33ed19335
yassboss <kyh58221022@gmail.com>
2021-12-16 00:05:58 +0900

    モデルの単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
-  NAME_READING_REGEX = /\A[ァ-ヶー]+\z/.freeze
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  VALID_NAME_READING_REGEX = /\A[ァ-ヶー]+\z/.freeze
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers', allow_blank: true
 
   with_options presence: true do
     validates :nickname, :birthday
-    validates :last_name, :first_name, format: {with: NAME_REGEX, message: 'には全角（漢字・ひらがな・カタカナ）を使用してください', allow_blank: true}
-    validates :last_name_reading, :first_name_reading, format: {with: NAME_READING_REGEX, message: 'には全角（カタカナ）を使用してください', allow_blank: true}
+    validates :last_name, :first_name, format: {with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters', allow_blank: true}
+    validates :last_name_reading, :first_name_reading, format: {with: VALID_NAME_READING_REGEX, message: 'is invalid. Input full-width katakana characters', allow_blank: true}
   end
 
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', allow_blank: true
 end
