bd9a0080eb189d5c57d30b7a3119585461952038
yassboss <kyh58221022@gmail.com>
2021-12-18 15:10:38 +0900

    Rubocopを実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,7 +67,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it '価格が300〜9999999の範囲になければ出品登録できない(10000000以上)' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
