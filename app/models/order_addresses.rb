f46325cd7166589496a7374ef9beb692c0f03857
yassboss <kyh58221022@gmail.com>
2021-12-19 16:23:28 +0900

    モデル、コントローラーの作成

--- app/models/order_addresses.rb
+++ app/models/order_addresses.rb
@@ -7,16 +7,17 @@ class OrderAddresses
 
   with_options presence: true do
     validates :item_id, :user_id, :city, :house_number, :order_id
-    validates :postal_code, format: { with: VALID_POSTAL_CODE_REGEX, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+    validates :postal_code, format: { with: VALID_POSTAL_CODE_REGEX, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :phone_number,
               numericality: { greater_than_or_equal_to: 10, less_than_or_equal_to: 11, only_integer: true,
-                              allow_blank: true},
+                              allow_blank: true },
               format: { with: VALID_PHONE_NUMBER_REGEX, allow_blank: true }
   end
   validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    Address.create(prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Address.create(prefecture: prefecture, city: city, house_number: house_number, building_name: building_name,
+                   phone_number: phone_number, order_id: order.id)
   end
-end 
+end
