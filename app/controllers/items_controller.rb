d6ed217931306a37f239ea6bc38c70e6a4fe9a22
yassboss <kyh58221022@gmail.com>
2021-12-19 12:38:01 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_root_path, only: :edit
+  before_action :move_to_root_path, only: [:edit, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -35,11 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-      render :show
-    end
+    @item.destroy
+    redirect_to root_path
   end
 
   private
