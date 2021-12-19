a65ab0533e99d2e64f7f821a544be869de24f8f1
yassboss <kyh58221022@gmail.com>
2021-12-19 20:21:17 +0900

    フォームの作成、保存処理・エラー表示実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,24 @@
 class OrdersController < ApplicationController
+
   def index
-    @order_address = OrderAddresses.new
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(item_id: @item.id, user_id: current_user.id)
   end
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,22 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+
+  VALID_POSTAL_CODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
+
+  with_options presence: true do
+    validates :postal_code, format: { with: VALID_POSTAL_CODE_REGEX, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :item_id, :user_id, :city, :house_number
+    validates :phone_number,
+              length: { in: 10..11, message: 'is too short', allow_blank: true },
+              numericality: { only_integer: true, allow_blank: true }
+  end
+  
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name,
+                   phone_number: phone_number, order_id: order.id)
+  end
+end
