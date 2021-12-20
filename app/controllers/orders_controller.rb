4d2e5bbe3224c71c5d3aaaf70d3b15a79b1cda40
yassboss <kyh58221022@gmail.com>
2021-12-20 22:13:28 +0900

    クレジットカード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -19,7 +20,16 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(
-      item_id: @item.id, user_id: current_user.id
+      item_id: @item.id, user_id: current_user.id, token: params[:token], price: @item.price
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
     )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,10 +1,12 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token,
+                :price
 
   VALID_POSTAL_CODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: { with: VALID_POSTAL_CODE_REGEX, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :item_id, :user_id, :city, :house_number
