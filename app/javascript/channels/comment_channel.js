d8a6829a58794270f425d343af891c796df07345
yassboss <kyh58221022@gmail.com>
2022-01-03 17:07:57 +0900

    日本時間表示の記述

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -16,12 +16,25 @@ if(location.pathname.match(/\/items\/\d/)){
     },
 
     received(data) {
+      function getTime(timed) {
+        var time = new Date(timed);
+        var year = time.getFullYear();
+        var mon = ("0"+(time.getMonth()+1)).slice(-2); //１を足すこと
+        var day = ("0"+time.getDate()).slice(-2);
+        var hour = ("0"+time.getHours()).slice(-2);
+        var min = ("0"+time.getMinutes()).slice(-2);
+        var sec = ("0"+time.getSeconds()).slice(-2);
+      
+        var s = year + "/" + mon + "/" + day + " " + hour + ":" + min + ":" + sec; 
+        return s;
+      }
+      
       const html = `
         <div class="comment">
           <p>${data.user.nickname}</p>
           <div class="comment-frame">
             <p>${data.comment.text}</p>
-            <p class="created">${data.comment.created_at}</p>
+            <p class="created">${getTime(data.comment.created_at)}</p>
           </div>
         </div>`
       const comments = document.getElementById("comments")
