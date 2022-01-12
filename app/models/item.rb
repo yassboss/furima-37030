0eecd465f8c4d08d7020c4043358fb1c2dcd1779
yassboss <kyh58221022@gmail.com>
2022-01-12 20:10:21 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
     temp_ids.each do |temp_id|
       save_notification_comment!(current_user, comment_id, temp_id['user_id'])
     end
-    save_notification_comment!(current_user, comment_id, user_id) unless temp_ids.include?(user.id)
+    save_notification_comment!(current_user, comment_id, user_id) unless temp_ids.include?(user_id: user.id)
   end
 
   def save_notification_comment!(current_user, comment_id, visited_id)
